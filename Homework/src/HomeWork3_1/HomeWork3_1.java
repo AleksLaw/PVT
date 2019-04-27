771bf7c49a20777b1d4013519b0bbcc2409a3d72
Law <aleksandr.law@gmail.com>
2019-04-27 23:29:30 +0300

    Homework and Lessons

---
+++ Homework/src/HomeWork3_1/HomeWork3_1.java
@@ -0,0 +1,17 @@
+package HomeWork3_1;
+//Задание
+//public static void main(String[] args) {
+//        int s = 3700;
+//        int sec;
+//        int m;
+//        int min, h;
+//        sec = s % 60;
+//        m = (s -sec) / 60;
+//        min = m % 60;
+//        h = (m -min) / 60;
+//        System.out.println(h+" часов" + min + " минут" + sec + " секунд");
+//        }
+//Продолжите приведенный пример, добавьте расчет и вывод суток и недель.
+
+public class HomeWork3_1 {
+}
