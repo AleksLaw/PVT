339c71a543dc8d768a55e995b00392201d18073c
Law <aleksandr.law@gmail.com>
2019-04-28 17:45:39 +0300

    Homework and Lessons

--- Homework/src/HomeWork2_1/Main.java
+++ Homework/src/HomeWork2_1/Main.java
@@ -7,4 +7,17 @@ package HomeWork2_1;
 
 public class Main {
 
+
+    public static void main(String[] args) {
+        Car car1 = new Car("red", "bmw", "x6", "gas" ,2000,200);
+        System.out.println(car1);
+        car1.start();
+        car1.driveForvard();
+        car1.stop();
+        Car car2 = new Car("green", "wv", "bee", "gas" ,2001,20);
+        System.out.println(car2);
+        car2.start();
+        car2.driveForvard();
+        car2.stop();
+    }
 }
