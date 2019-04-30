dfb36023c7f43f0e3228d833483c9ddd23788028
Law <aleksandr.law@gmail.com>
2019-05-01 00:51:22 +0300

    Homework and Lessons

--- Homework/src/HomeWork09/HomeWork09.java
+++ Homework/src/HomeWork09/HomeWork09.java
@@ -1,4 +1,26 @@
 package HomeWork09;
+//Имеется последовательность чисел найти минимум и максимум
 
 public class HomeWork09 {
+    public static void main(String[] args) {
+
+        int x = (int) (Math.random() * 20);
+        int min = x;
+        int max = x;
+
+        while (x != 0) {
+
+            if (x > max) {
+                max = x;
+            }
+            if (x < min) {
+                min = x;
+            }
+
+            x = (int) (Math.random() * 20);
+
+        }
+        System.out.println("min " + min);
+        System.out.println("max " + max);
+    }
 }
