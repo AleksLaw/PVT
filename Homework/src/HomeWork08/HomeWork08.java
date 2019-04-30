803c7bd75654cb6a2677791a8be33d0b8d38ebf8
Lysik Aleksandr <aleksandr.law@gmail.com>
2019-04-30 14:21:56 +0300

    Homework and Lessons

---
+++ Homework/src/HomeWork08/HomeWork08.java
@@ -0,0 +1,28 @@
+package HomeWork08;
+
+//Задание
+//Используя цикл while, рассчитайте для введенного числа его факториал.
+
+import java.util.Scanner;
+
+public class HomeWork08 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numder");
+        String q = scanner.nextLine();
+
+        int a = Integer.parseInt(q);
+        int b = 1;
+        int i = 1;
+        while (i <= a) {
+            b = b * i;
+            i++;
+        }
+        System.out.println("Факториал числа " + a + " равен " + b);
+
+    }
+
+}
+
+
