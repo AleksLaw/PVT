36ce5f8c8d40432249d6592c49059f75b325aa6f
Law <aleksandr.law@gmail.com>
2019-05-01 19:20:24 +0300

    Homework and Lessons

---
+++ Homework/src/Homework17/Homework16.java
@@ -0,0 +1,41 @@
+package Homework17;
+//Задание
+// Создать двумерный массив в виде бабочки
+// Упарился решил с помощью гугла, смысл понял повторить не смогу))
+
+
+public class Homework16 {
+    public static void main(String[] args) {
+        int bat[][] = new int[5][5];
+        int i, j;
+
+
+        for (i = 0; i < bat.length / 2; i++) {
+            for (j = 0; j < bat[i].length; j++) {
+                if ((j < i) || (j >= (bat[i].length - i)))
+                    bat[i][j] = 0;
+                else
+                    bat[i][j] = 1;
+
+            }
+
+        }
+        for (i = bat.length - 1; i >= bat.length / 2; i--) {
+            for (j = 0; j < bat[i].length; j++) {
+                if ((j < (bat[i].length - 1 - i)) || (j > i))
+                    bat[i][j] = 0;
+                else
+                    bat[i][j] = 1;
+            }
+        }
+        for (i = 0; i < bat.length; i++) {
+            for (j = 0; j < bat[i].length; j++) {
+                System.out.print(bat[i][j]);
+            }
+            System.out.println(" ");
+        }
+    }
+
+
+}
+
