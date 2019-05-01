86ead27f68457ad538669f5f8b4cf450f4737c1f
Law <aleksandr.law@gmail.com>
2019-05-01 23:15:09 +0300

    Homework and Lessons

--- Homework/src/Homework19/Homework19.java
+++ Homework/src/Homework19/Homework19.java
@@ -1,19 +1,24 @@
 package Homework19;
+
 //Задание
 // Посчитать слова, не учитывать пробелы в начале и конце и двойные пробелы
 public class Homework19 {
     public static void main(String[] args) {
-        int count = 1;
+        int count = 0;
 
         String q = " «Ничего  ты           не знаешь, Джон  Сноу!  вздохнула    она и умерла.»       ";
 
-        while (q.contains("  ")) {
+
+        while (q.contains("  ")) {   //замена 2 пробела на 1
             String s = q.replace("  ", " ");
             q = s;
         }
 
-        String w = q.trim();
+        String w = q.trim(); //первый и последний пробел
 
+        if (w.length() > 0) { //проверка есть ли вообще слова
+            count = 1;
+        }
         for (int i = 0; i < w.length(); i++) {
             if (w.charAt(i) == ' ') {
                 count++;
