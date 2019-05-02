fd0547584625c1d169d12bea94c5f593ed1e678d
Law <aleksandr.law@gmail.com>
2019-05-03 00:16:23 +0300

    Homework and Lessons

--- Homework/src/Homework23/Main.java
+++ Homework/src/Homework23/Main.java
@@ -2,17 +2,17 @@ package Homework23;
 
 public class Main {
     public static void main(String[] args) {
-        Bankomat bankomat = new Bankomat(100,100,0);
+        Bankomat bankomat = new Bankomat(100,100,100);
         bankomat.getTotalSumm();
 
-//        bankomat.add$20(0);
-//        bankomat.add$50(0);
-//        bankomat.add$100(0);
-  //      bankomat.getTotalSumm();
+        bankomat.add$20(0);
+        bankomat.add$50(0);
+        bankomat.add$100(0);
+        bankomat.getTotalSumm();
         bankomat.get$100();
 
 
-        //bankomat.getMoneyYesNo(140);
+        bankomat.getMoneyYesNo(140);
         bankomat.getMoney(1180);
         bankomat.getTotalSumm();
 
