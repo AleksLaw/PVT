2e9f9d1ca3bf4184272c4b79ac3f535ff851a4a5
Lysik Aleksandr <aleksandr.law@gmail.com>
2019-05-03 13:27:27 +0300

    Homework and Lessons

--- Homework/src/Homework23/Main.java
+++ Homework/src/Homework23/Main.java
@@ -2,7 +2,7 @@ package Homework23;
 
 public class Main {
     public static void main(String[] args) {
-        Bankomat bankomat = new Bankomat(1,3,0);
+        Bankomat bankomat = new Bankomat(100,100,100);
         bankomat.getTotalSumm();
 
         bankomat.add$20(0);
@@ -12,8 +12,41 @@ public class Main {
         bankomat.get$100();
 
 
-        bankomat.getMoneyYesNo(170);
+        bankomat.getMoneyYesNo(190);
+        bankomat.getMoney(20);
+        bankomat.getMoney(40);
+        bankomat.getMoney(60);
+        bankomat.getMoney(70);
+        bankomat.getMoney(80);
+        bankomat.getMoney(90);
+        bankomat.getMoney(100);
+        bankomat.getMoney(120);
+        bankomat.getMoney(140);
+        bankomat.getMoney(50);
+        bankomat.getMoney(150);
+        bankomat.getMoney(160);
         bankomat.getMoney(170);
+        bankomat.getMoney(180);
+        bankomat.getMoney(190);
+        bankomat.getMoney(200);
+        bankomat.getMoney(220);
+        bankomat.getMoney(240);
+        bankomat.getMoney(250);
+        bankomat.getMoney(260);
+        bankomat.getMoney(270);
+        bankomat.getMoney(280);
+        bankomat.getMoney(290);
+        bankomat.getMoney(300);
+        bankomat.getMoney(320);
+        bankomat.getMoney(340);
+        bankomat.getMoney(350);
+        bankomat.getMoney(360);
+        bankomat.getMoney(380);
+        bankomat.getMoney(370);
+        bankomat.getMoney(390);
+        bankomat.getMoney(400);
+        bankomat.getMoney(420);
+        bankomat.getMoney(440);
         bankomat.getTotalSumm();
 
 
