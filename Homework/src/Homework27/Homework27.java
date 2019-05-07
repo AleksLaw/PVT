069d9e10286b34a217e9b52acfad2ee715457485
aleks <aleksandr.law@gmail.com>
2019-05-07 21:30:41 +0300

    Homework and Lessons

--- Homework/src/Homework27/Homework27.java
+++ Homework/src/Homework27/Homework27.java
@@ -3,8 +3,28 @@ package Homework27;
 //Создать коллекцию, заполнить случайными числами
 //Удалить повторяющиеся числа
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Homework27 {
     public static void main(String[] args) {
 
+        int listSize = 20;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < listSize; i++) {
+            list.add((int) (Math.random() * 20 + 1));
+        }
+
+        System.out.println("С дубликатами:" +list);
+
+        Set<Integer> listDublicat = new HashSet<>();
+
+        listDublicat.addAll(list); //можно просто распечатать  не возвращая значения в list
+        list.clear();
+        list.addAll(listDublicat);
+
+        System.out.println("Без дубликатов:" +list);
     }
 }
