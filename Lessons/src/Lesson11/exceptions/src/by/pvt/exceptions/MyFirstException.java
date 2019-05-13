ecc287752fd2cd099954de227fe8396147498b2e
Lysik Aleksandr <aleksandr.law@gmail.com>
2019-05-13 18:30:21 +0300

    Homework and Lessons

---
+++ Lessons/src/Lesson11/exceptions/src/by/pvt/exceptions/MyFirstException.java
@@ -0,0 +1,8 @@
+package Lesson11.exceptions.src.by.pvt.exceptions;
+
+public class MyFirstException extends Exception {
+
+    public MyFirstException(String message) {
+        super(message);
+    }
+}
---
+++ Lessons/src/Lesson12/src/by/pvt/serial/ExampleSerial.java
@@ -0,0 +1,45 @@
+package Lesson12.src.by.pvt.serial;
+
+import java.io.*;
+
+/**
+ *
+ */
+
+class Car implements Serializable {
+    private static final long serialVersionUID = 2L;
+
+    String name;
+    int price;
+    int weight;
+
+    static int year = 2016;
+}
+
+public class ExampleSerial {
+
+    public static void main(String[] args) {
+        try {
+            ObjectOutputStream objectOutputStream =
+                    new ObjectOutputStream(new FileOutputStream("car.serial"));
+
+            Car car1 = new Car(); //1 option! how to create object instance
+            car1.name = "BMW X6";
+            car1.price = 150000;
+            Car.year = 2016;
+            objectOutputStream.writeObject(car1); //serialization
+            objectOutputStream.flush();
+            objectOutputStream.close();
+
+            ObjectInputStream objectInputStream =
+                    new ObjectInputStream(new FileInputStream("car.serial"));
+            Object obj = objectInputStream.readObject(); //deserialization
+            Car car2 = (Car)obj;
+            System.out.println(car2.name + " " + car2.price + " " + car2.year);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
