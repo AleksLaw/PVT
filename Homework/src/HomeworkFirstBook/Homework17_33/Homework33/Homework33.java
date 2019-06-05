03e0f0784ad11081444295b5108f50c0e0d325d1
law <aleksandr.law@gmail.com>
2019-06-05 13:34:17 +0300

    Homework and Lessons

--- Homework/src/HomeworkFirstBook/Homework17_33/Homework33/Homework33.java
+++ Homework/src/HomeworkFirstBook/Homework17_33/Homework33/Homework33.java
@@ -1,25 +1,65 @@
 package HomeworkFirstBook.Homework17_33.Homework33;
 // Задание
-// Вывести список файлов и катологов выбранного каталого на диске.
-// Файлы и каталоги должны быть распечатаны отдельно
+// Вывести список файлов и катологов выбранного каталого на диске. Используя Thread
+// Написал для двух потоков, Не очень понимаю условия задачи.
 
 import java.io.File;
 
-public class Homework33 {
-    public static void main(String[] args) {
-        File folder = new File("C:/Windows");
-        File[] listOfFiles = folder.listFiles();
-        System.out.println("Список каталогов");
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isDirectory()) {
-                System.out.println("Каталог " + listOfFiles[i].getName());
+public class Homework33 implements Runnable {
+    static String dir = "e:/1";
+    volatile File folder = new File(dir);
+    volatile File[] listOfFiles;
+    private boolean isDirBusy = true;
+    private boolean isFileBusy = true;
+
+
+    private synchronized void getFile(File[] d) {
+        try {
+            while (isDirBusy) {
+
+                for (int i = 0; i < d.length; i++) {
+                    if (d[i].isFile()) {
+                        System.out.println("Файл " + d[i].getName() + "   " + Thread.currentThread().getName());
+                    }
+                }
+                isDirBusy = false;
+                break;
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println("\n"+"Список файлов");
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("Файл "+ listOfFiles[i].getName());
+    }
+
+    private synchronized void getDir(File[] s) {
+        try {
+            while (isFileBusy) {
+
+                for (int i = 0; i < s.length; i++) {
+                    if (s[i].isDirectory()) {
+                        System.out.println("Каталог " + s[i].getName() + "   " + Thread.currentThread().getName());
+                    }
+                }
+                isFileBusy = false;
+                break;
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Homework33()).start();
+        new Thread(new Homework33()).start();
+    }
+
+
+    @Override
+    public void run() {
+        listOfFiles = folder.listFiles();
+        if (Thread.currentThread().getName().equals("Thread-0")) {
+            getDir(listOfFiles);
+        } else {
+            getFile(listOfFiles);
         }
     }
 }
